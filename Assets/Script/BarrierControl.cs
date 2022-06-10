3e321d62179cea1c1eb854b19f120088810000fc
将希 山田 <k019g1369@g.neec.ac.jp>
2022-06-10 16:16:53 +0900

    バリア準備

--- Assets/Script/BarrierControl.cs
+++ Assets/Script/BarrierControl.cs
@@ -4,52 +4,60 @@ using UnityEngine;
 
 public class BarrierControl : MonoBehaviour
 {
+    
     Vector3 target { get; set; }
     Vector3 move { get; set; }
     bool isOpen { get; set; }
     [SerializeField] private GameObject barrierObject;
     private GameObject barrieClone;
-    //”¼Œa
+    //åŠå¾„
     [SerializeField] private float radius;
-    //‚‚ğ“WŠJ‚·‚é’·‚³
+    //å±•é–‹ã™ã‚‹é•·ã•(æ™‚é–“)
     [SerializeField] private int openSpan;
-    //“WŠJ‚µ‚Ä‚¢‚é’·‚³
+    //
+    [SerializeField] private int angle=0;
+    //å±•é–‹ã—ã¦ã‹ã‚‰ã®çµŒéæ™‚é–“
     int time;
 
     // Start is called before the first frame update
     void Start()
     {
         target = new Vector3();
+        time=0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //“WŠJ’†
+        //ç”Ÿãã¦ã„ã‚‹é–“
         if (time < openSpan)
         {
             time++;
         }
-        //“WŠJ‚µ‚Ä‚È‚¢ŠÔ
         else
         {
             if (isOpen)
             {
                 isOpen = false;
-                Destroy(barrieClone);
+               //Destroy(barrieClone);
                 time = 0;
             }
         }
-        //‘Ì‚ÌŒü‚«‚ğmove‚©‚ç‹‚ß‚é
-        float angle = Mathf.Atan2(move.x, move.z);
-        Vector3 
-        //ƒoƒŠƒA‚ğ“WŠJ‚·‚éÀ•W
+        //move ãƒ™ã‚¯ãƒˆãƒ«ã‚’åº¦æ•°æ³•ã«å¤‰æ›
+        //float angle = Mathf.Atan2(move.x, move.z);
+        Vector3 barrierPosition = target + new Vector3(Mathf.Sin(angle) * radius, 0.0f, Mathf.Cos(angle) * radius);
+
+       //ã‚¹ãƒãƒ¼ãƒ³
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            barrieClone = Instantiate(barrierObject, new Vector3(-5.0f, 0.0f, 0.0f), new Quaternion(0, 1, 0, angle));
+            isOpen = true;
+            barrieClone = Instantiate(barrierObject, barrierPosition, new Quaternion(0, 1, 0, angle));
         }
-
-        barrieClone.transform.rotation = new Quaternion(0, 1, 0, angle);
-        barrieClone. transform.position = );
+       //å±•é–‹ä¸­ãªã‚‰
+        if (isOpen)
+        {
+            barrieClone.transform.rotation = new Quaternion(0, 1, 0, angle);
+            barrieClone.transform.position = barrierPosition;
+        }
     }
 }
