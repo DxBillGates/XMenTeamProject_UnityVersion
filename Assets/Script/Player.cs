149d429c020ae06c5e24d3c6c036220b7da3414a
DxBillGates <suzu.tomo283@gmail.com>
2022-06-11 08:34:37 +0900

    onTriggerで壁ずり移動ができるように修正

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float movePower;
-    private Vector3 velocity;
 
-    [SerializeField] private Vector3 beforeDirection;
-    [SerializeField] private Vector3 currentDirection;
+    private Vector3 velocity;
+    private Vector3 currentDirection;
 
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
@@ -24,21 +24,20 @@ public class Player : MonoBehaviour
         RotateDirection();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         switch (other.gameObject.tag)
         {
             case "Wall":
                 // ヒットした障害物のヒットした法線方向に押し出したいからその法線を取得
                 Vector3 hitNormal = other.transform.forward;
-                Vector3 pos = transform.position - velocity;
 
                 // 座標を位置フレーム前に戻す
-                transform.position = pos;
+                const float PUSH_VALUE = 1.5f;
+                transform.position -= velocity * PUSH_VALUE;
 
                 // 壁ずりベクトルを計算
                 Vector3 moveVector = velocity - Vector3.Dot(velocity, hitNormal) * hitNormal;
-                moveVector *= velocity.magnitude;
                 transform.position += moveVector;
                 break;
             case "Ball":
@@ -52,22 +51,23 @@ public class Player : MonoBehaviour
     // 移動処理
     private void Move()
     {
-        beforeDirection = currentDirection;
-
-        Vector3 moveVector = new Vector3(Input.GetAxisRaw("Horizontal"),0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         bool isInput;
 
-        velocity += movePower * Time.deltaTime * moveVector.normalized;
+        velocity = movePower * moveVector.normalized;
 
         transform.position += velocity;
 
         isInput = inputDirection.magnitude > 0.5f;
-        if(isInput)currentDirection = inputDirection.normalized;
+        if (isInput) currentDirection = inputDirection.normalized;
     }
 
+    // プレイヤーを移動ベクトル方向に向かせる
     private void RotateDirection()
     {
+        if (currentDirection.magnitude == 0) return;
+
         transform.rotation = Quaternion.LookRotation(currentDirection);
     }
 }
