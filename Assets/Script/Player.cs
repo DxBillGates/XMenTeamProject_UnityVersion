331efdb35edbe7cf2ece61bdd11bed371516ffde
DxBillGates <suzu.tomo283@gmail.com>
2022-06-10 13:24:16 +0900

    rigidbodyのvelocityを無効にするコンポーネントを用意 プレイヤーの基本的な移動を実装

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Vector3 velocity;
+    [SerializeField] private float movePower;
+    private Vector3 velocity;
+    public Vector3 currentDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        velocity = new Vector3();
+
+        Move();
+        RotateDirection();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    // ˆÚ“®ˆ—
+    private void Move()
     {
-        
+        Vector3 moveVector = new Vector3(Input.GetAxisRaw("Horizontal"),0, Input.GetAxisRaw("Vertical"));
+        Vector3 inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        bool isInput;
+
+        velocity += movePower * Time.deltaTime * moveVector.normalized;
+
+        transform.position += velocity;
+
+        isInput = inputDirection.magnitude > 0.5f;
+        if(isInput)currentDirection = inputDirection.normalized;
+    }
+
+    private void RotateDirection()
+    {
+        transform.rotation = Quaternion.LookRotation(currentDirection);
     }
 }
