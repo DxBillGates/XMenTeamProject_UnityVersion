5fb80eb7eaee191463f54e02a83a764c399e3889
GUTTYman <77372850+GUTTYman@users.noreply.github.com>
2022-06-11 12:09:20 +0900

    Revert "barrier調整"
    This reverts commit 62bc7332b5ee50cec9b100b529de661400a35505.

--- Assets/Script/BarrierControl.cs
+++ Assets/Script/BarrierControl.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class BarrierControl : MonoBehaviour
 {
+    
     Vector3 target { get; set; }
-    Vector3 directioon { get; set; }
+    Vector3 move { get; set; }
     bool isOpen { get; set; }
-    //盾のモデルをセットする要オブジェクト
+    //盾のモデルをセット
     [SerializeField] private GameObject barrierObject;
     //スポーンさせるクローン的なオブジェクト
     private GameObject barrieClone;
@@ -21,8 +22,8 @@ public class BarrierControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        target = Vector3.zero;
-        time = 0;
+        target = new Vector3();
+        time=0;
     }
 
     // Update is called once per frame
@@ -38,31 +39,26 @@ public class BarrierControl : MonoBehaviour
             if (isOpen)
             {
                 isOpen = false;
-                Destroy(barrieClone);
+               //Destroy(barrieClone);
                 time = 0;
             }
         }
-        //directionベクトルを度数法に変換
-        float angle = Mathf.Atan2(directioon.x, directioon.z);
+        //move ベクトルを度数法に変換
+        //float angle = Mathf.Atan2(move.x, move.z);
+        float angle=(float)time/10.0f;
         Vector3 barrierPosition = target + new Vector3(Mathf.Sin(angle) * radius, 0.0f, Mathf.Cos(angle) * radius);
 
-        //スポーン
-        if (!isOpen)
+       //スポーン
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                isOpen = true;
-                                                                         //この角度指定間違いなのでどうにかしたい
-                barrieClone = Instantiate(barrierObject, barrierPosition, Quaternion.Euler(0, angle, 0));
-            }
+            isOpen = true;
+            barrieClone = Instantiate(barrierObject, barrierPosition, new Quaternion(0, 1, 0, angle));
         }
-        //展開中なら
+       //展開中なら
         if (isOpen)
         {
-            //バリアのforwordがtargetに向いてるので注意
-            barrieClone.transform.LookAt(target);
+            barrieClone.transform.rotation = new Quaternion(0, 1, 0, angle);
             barrieClone.transform.position = barrierPosition;
-            
         }
     }
 }
