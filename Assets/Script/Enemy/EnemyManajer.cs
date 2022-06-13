a7d4c3f8349469b4796aa42fe60f27c59b819cc3
rc39wpbxs <rc39wpbxs7mt7rtmzdmb@gmail.com>
2022-06-13 10:43:00 +0900

    敵の出現範囲(半径)を指定できるように プレイヤーの周りに出現できないように

--- Assets/Script/Enemy/EnemyManajer.cs
+++ Assets/Script/Enemy/EnemyManajer.cs
@@ -11,6 +11,13 @@ public class EnemyManajer : MonoBehaviour
     //最初に出す数
     [SerializeField] private int MaxBarrierEnemyNum = 1;
     [SerializeField] private int MaxFollowEnemyNum = 1;
+
+    //　敵を出現させる範囲
+    [SerializeField] private float Spawn_Range = 30;
+    //　プレイヤーからどれくらいの範囲に敵が出現できないか
+    [SerializeField] private float Cant_Spawn_Distance = 10;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +25,52 @@ public class EnemyManajer : MonoBehaviour
         for (int i = 0; i < MaxBarrierEnemyNum; ++i)
         {
             //引数の真ん中変更で生成するポジション変更
-            Instantiate(BarrierEnemy, new Vector3(), Quaternion.identity);
+
+            // ランダムなポジションの設定
+            Vector3 spawnPos =
+                new Vector3(Random.Range(-1 * Spawn_Range, Spawn_Range), 0, Random.Range(-1 * Spawn_Range, Spawn_Range));
+
+            while (true)
+            {
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+                // 出現禁止範囲内なら位置を変更
+                if (Vector3.Distance(player.transform.position, spawnPos) <= Cant_Spawn_Distance)
+                {
+                    spawnPos = 
+                        new Vector3(Random.Range(-1 * Spawn_Range, Spawn_Range), 0, Random.Range(-1 * Spawn_Range, Spawn_Range));
+
+                }
+                else break;
+                
+            }
+
+            Instantiate(BarrierEnemy, spawnPos, Quaternion.identity);
         }
         //追っかけエネミー生成
         for (int i = 0; i < MaxFollowEnemyNum; ++i)
         {
+            Vector3 spawnPos = 
+                new Vector3(Random.Range(-1 * Spawn_Range, Spawn_Range),0, Random.Range(-1 * Spawn_Range, Spawn_Range));
+
+            while (true)
+            {
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+                // 出現禁止範囲内なら位置を変更
+                if (Vector3.Distance(player.transform.position, spawnPos) <= Cant_Spawn_Distance)
+                {
+                    spawnPos =
+                        new Vector3(Random.Range(-1 * Spawn_Range, Spawn_Range), 0, Random.Range(-1 * Spawn_Range, Spawn_Range));
+
+
+                }
+                else break;
+
+            }
+
             //引数の真ん中変更で生成するポジション変更
-            Instantiate(FollowEnemy, new Vector3(), Quaternion.identity);
+            Instantiate(FollowEnemy, spawnPos, Quaternion.identity);
         }
     }
 
