807ae3c613cadba4ecf2245b4e14d24fbf1098bc
GUTTYman <77372850+GUTTYman@users.noreply.github.com>
2022-06-17 00:04:09 +0900

    壁の色を変えるスクリプト準備

---
+++ Assets/ChangeMaterial.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    //通常時のマテリアル
+    [SerializeField] private Material normalMat;
+    //アルティメットスキル展開時のマテリアル(回りを暗くする用)
+    [SerializeField] private Material blackMat;
+    private GameObject gameObject;
+
+    private MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject = GetComponent<GameObject>();
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        meshRenderer.material = normalMat;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetNormalMaterial()
+    {
+        //普通のマテリアルをセット
+        meshRenderer.material = normalMat;
+    }
+    public void SetBlackMaterial()
+    {
+        //黒いマテリアルをセット
+        meshRenderer.material = blackMat;
+
+    }
+}
