03e5f2742a3166db965d54fc9c31437a495e0c27
GUTTYman <77372850+GUTTYman@users.noreply.github.com>
2022-06-17 00:20:56 +0900

    Unity保存し忘れ

--- Assets/ChangeMaterial.cs
+++ Assets/ChangeMaterial.cs
@@ -4,37 +4,37 @@ using UnityEngine;
 
 public class ChangeMaterial : MonoBehaviour
 {
-    //通常時のマテリアル
-    [SerializeField] private Material normalMat;
-    //アルティメットスキル展開時のマテリアル(回りを暗くする用)
-    [SerializeField] private Material blackMat;
-    private GameObject gameObject;
+    ////通常時のマテリアル
+    //[SerializeField] private Material normalMat;
+    ////アルティメットスキル展開時のマテリアル(回りを暗くする用)
+    //[SerializeField] private Material blackMat;
+    //private GameObject gameObject;
 
     private MeshRenderer meshRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject = GetComponent<GameObject>();
-        meshRenderer = GetComponent<MeshRenderer>();
+        //gameObject = GetComponent<GameObject>();
+        //meshRenderer = GetComponent<MeshRenderer>();
 
-        meshRenderer.material = normalMat;
+        //meshRenderer.material = normalMat;
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    //void Update()
+    //{
         
-    }
-    public void SetNormalMaterial()
-    {
-        //普通のマテリアルをセット
-        meshRenderer.material = normalMat;
-    }
-    public void SetBlackMaterial()
-    {
-        //黒いマテリアルをセット
-        meshRenderer.material = blackMat;
+    //}
+    //public void SetNormalMaterial()
+    //{
+    //    //普通のマテリアルをセット
+    //    meshRenderer.material = normalMat;
+    //}
+    //public void SetBlackMaterial()
+    //{
+    //    //黒いマテリアルをセット
+    //    meshRenderer.material = blackMat;
 
-    }
+    //}
 }
