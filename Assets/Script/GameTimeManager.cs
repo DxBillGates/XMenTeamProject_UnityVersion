c1eee9e2cc6a7bb5a8a628454c6c88ba07363495
DxBillGates <suzu.tomo283@gmail.com>
2022-06-17 15:34:02 +0900

    GameTimeManagerの謎バグ修正

---
+++ Assets/Script/GameTimeManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimeManager : SingletonComponent<GameTimeManager>
+{
+    // ƒQ[ƒ€“àŠÔ 0 ~ 1
+    [SerializeField, Range(0, 1)] private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 1;
+    }
+
+    void Update()
+    {
+        UltimateSkillManager skillManager = UltimateSkillManager.GetInstance();
+        time = 1;
+        if (skillManager.GetActiveFlagController().activeType == FlagActiveType.PRE)
+        {
+            time = 0.05f;
+        }
+    }
+
+    public float GetTime()
+    {
+        return time;
+    }
+
+    public void SetTime(float value)
+    {
+        // 0 ~ 1‚ğ’´‚¦‚½‚è‰º‰ñ‚ç‚È‚¢‚æ‚¤‚ÉüŒ`•âŠÔ
+        value = Mathf.Lerp(0, 1, value);
+        time = value;
+    }
+}
