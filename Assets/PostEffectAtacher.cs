4855d9c90ef01640c0b0e970b24f9ff5727a0bdc
GUTTYman <77372850+GUTTYman@users.noreply.github.com>
2022-06-28 16:25:07 +0900

    ブルーム変更

--- Assets/PostEffectAtacher.cs
+++ Assets/PostEffectAtacher.cs
@@ -13,14 +13,26 @@ public class PostEffectAtacher : MonoBehaviour
         RenderTexture highLumiTex =RenderTexture.GetTemporary(source.width, source.width, 0, source.format);
         RenderTexture blurTex =RenderTexture.GetTemporary(source.width, source.width, 0, source.format);
         //縮小バッファ  
-        RenderTexture buffer=RenderTexture.GetTemporary(blurTex.width/2,blurTex.height/2,0,source.format);
+        RenderTexture buffer1=RenderTexture.GetTemporary(source.width/2, source.height/2,0, source.format);
+        RenderTexture buffer2=RenderTexture.GetTemporary(source.width/4, source.height/4,0, source.format);
+        RenderTexture buffer3=RenderTexture.GetTemporary(source.width/8, source.height/8,0, source.format);
         
 
         Graphics.Blit(source, highLumiTex, highLumiMat);
-        Graphics.Blit(highLumiTex, blurTex, blurMat);
-        compoMat.SetTexture("_HighLumi", blurTex);
+        Graphics.Blit(highLumiTex, buffer1);
+        Graphics.Blit(buffer1, buffer2, blurMat);
+        Graphics.Blit(buffer2, buffer3, blurMat);
+        Graphics.Blit(buffer3, blurTex);
+        Graphics.Blit(blurTex, buffer3);
+        Graphics.Blit(buffer3, buffer2);
+        Graphics.Blit(buffer2, buffer1);
+
+        compoMat.SetTexture("_HighLumi", buffer1);
         Graphics.Blit(source, destination, compoMat);
 
+        RenderTexture.ReleaseTemporary(buffer1);
+        RenderTexture.ReleaseTemporary(buffer2);
+        RenderTexture.ReleaseTemporary(buffer3);
         RenderTexture.ReleaseTemporary(blurTex);
         RenderTexture.ReleaseTemporary(highLumiTex);
     }
