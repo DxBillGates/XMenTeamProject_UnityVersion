c88f0b046cd2f09dc17c9b0c1e3188a05fdaaf6f
GUTTYman <77372850+GUTTYman@users.noreply.github.com>
2022-06-30 22:46:59 +0900

    いらないもの削除

--- Assets/PostEffectAtacher.cs
+++ Assets/PostEffectAtacher.cs
@@ -4,24 +4,10 @@ using UnityEngine;
 
 public class PostEffectAtacher : MonoBehaviour
 {
-    public Material highLumiMat;
-    public Material blurMat;
-    public Material compoMat;
+    public Material shaderMaterial;
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        RenderTexture highLumiTex =RenderTexture.GetTemporary(source.width, source.width, 0, source.format);
-        RenderTexture blurTex =RenderTexture.GetTemporary(source.width, source.width, 0, source.format);
-        //縮小バッファ  
-        RenderTexture buffer=RenderTexture.GetTemporary(blurTex.width/2,blurTex.height/2,0,source.format);
-        
-
-        Graphics.Blit(source, highLumiTex, highLumiMat);
-        Graphics.Blit(highLumiTex, blurTex, blurMat);
-        compoMat.SetTexture("_HighLumi", blurTex);
-        Graphics.Blit(source, destination, compoMat);
-
-        RenderTexture.ReleaseTemporary(blurTex);
-        RenderTexture.ReleaseTemporary(highLumiTex);
+        Graphics.Blit(source, destination, shaderMaterial);
     }
 }
