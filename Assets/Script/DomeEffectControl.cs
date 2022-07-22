9d0929f5f214586efb934f1de478d90a7107c2f6
将希 山田 <k019g1369@g.neec.ac.jp>
2022-07-22 14:23:05 +0900

    PostEffectManager.instance消した

--- Assets/Script/DomeEffectControl.cs
+++ Assets/Script/DomeEffectControl.cs
@@ -28,8 +28,7 @@ public class DomeEffectControl : MonoBehaviour
     private void Update()
     {
         bloom.color.Override(color);
-        //　ポストプロセスボリュームに反映
-        postProcessVolume = PostProcessManager.instance.QuickVolume(gameObject.layer, 0f, bloom);
+
     }
     void OnDestroy()
     {
